2fe9336467970b092f3fa055b41c2a1bdba02ec7
torok-zeus <fridrichakos2@gmail.com>
2026-04-16 11:49:04 +0200

    10parkoló

--- Client.fs
+++ Client.fs
@@ -5,34 +5,26 @@ open WebSharper.JavaScript
 open WebSharper.UI
 open WebSharper.UI.Client
 open WebSharper.UI.Templating
+open WebSharper.UI.Html
 
 [<JavaScript>]
 module Client =
-    // The templates are loaded from the DOM, so you just can edit index.html
-    // and refresh your browser, no need to recompile unless you add or remove holes.
-    type IndexTemplate = Template<"wwwroot/index.html", ClientLoad.FromDocument>
-
-    let People =
-        ListModel.FromSeq [
-            "John"
-            "Paul"
-        ]
-
 
     [<SPAEntryPoint>]
     let Main () =
-        let newName = Var.Create ""
+        let spots =
+            [1..10]
+            |> List.map (fun i -> "A" + string i)
 
-        IndexTemplate.Main()
-            .ListContainer(
-                People.View.DocSeqCached(fun (name: string) ->
-                    IndexTemplate.ListItem().Name(name).Doc()
-                )
-            )
-            .Name(newName)
-            .Add(fun _ ->
-                People.Add(newName.Value)
-                newName.Value <- ""
-            )
-            .Doc()
+        let renderSpot spot =
+            button [
+                attr.style "margin:5px; padding:15px; background-color:green; color:white; border none;"
+            ] [
+                text spot
+            ]
+
+        div [] [
+            h3 [] [text "Parking System"]
+            div [] (spots |> List.map renderSpot)
+        ]
         |> Doc.RunById "main"
